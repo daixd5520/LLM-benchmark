dea67409ecc433c182645f51cd9b8e9e9479037c
xindi dai <73395671+daixd5520@users.noreply.github.com>
2023-10-19 11:46:20 +0800

    输出时间和throughput

--- Baichuan2-7b-chat/cli_demo.py
+++ Baichuan2-7b-chat/cli_demo.py
@@ -6,21 +6,21 @@ from colorama import Fore, Style
 from tempfile import NamedTemporaryFile
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation.utils import GenerationConfig
-
+import time
 
 def init_model():
     print("init model ...")
     model = AutoModelForCausalLM.from_pretrained(
-        "baichuan-inc/Baichuan2-13B-Chat",
+        "/home/ubuntu/workspace/Baichuan2-7B-Chat",
         torch_dtype=torch.float16,
         device_map="auto",
         trust_remote_code=True
     )
     model.generation_config = GenerationConfig.from_pretrained(
-        "baichuan-inc/Baichuan2-13B-Chat"
+        "/home/ubuntu/workspace/Baichuan2-7B-Chat"
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        "baichuan-inc/Baichuan2-13B-Chat",
+        "/home/ubuntu/workspace/Baichuan2-7B-Chat",
         use_fast=False,
         trust_remote_code=True
     )
@@ -45,8 +45,12 @@ def vim_input():
 
 
 def main(stream=True):
+    model_start=time.time()
     model, tokenizer = init_model()
+    model_end=time.time()
+    
     messages = clear_screen()
+    print("模型加载时间：",model_end-model_start)
     while True:
         prompt = input(Fore.GREEN + Style.BRIGHT + "\n用户：" + Style.NORMAL)
         if prompt.strip() == "exit":
@@ -68,6 +72,7 @@ def main(stream=True):
             try:
                 for response in model.chat(tokenizer, messages, stream=True):
                     print(response[position:], end='', flush=True)
+                    
                     position = len(response)
                     if torch.backends.mps.is_available():
                         torch.mps.empty_cache()
@@ -79,6 +84,17 @@ def main(stream=True):
             print(response)
             if torch.backends.mps.is_available():
                 torch.mps.empty_cache()
+        #################改：增start##################
+        print("\ninput_length:" ,model.input_len)
+        print("\nTokens generated in 1 sec:",model.model.one_sec_tokens)
+        print("\nFirst token time(秒):",model.model.first_token_time)
+        print("\ngenerated tokens count:",model.model.fwd_num-1)
+        #print("\ntime_tot(秒):" ,model.model.time_tot)
+        print("\nms/token:",model.model.time_tot*1000/(model.model.fwd_num-1))
+        model.model.time_tot=0
+        model.model.fwd_num=0
+        model.model.one_sec_tokens=0
+        #################改：增end####################
         messages.append({"role": "assistant", "content": response})
     print(Style.RESET_ALL)
 
